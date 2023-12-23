fc71a098a3650d3fd52f4fe992f8b6e4c3c12112
HenrickyL <HenrickyL1@gmail.com>
2023-12-22 21:50:58 -0300

    add canvas base & styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useRef } from "react"
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  //main - Engine
+  useEffect(()=>{
+    if(!canvasRef.current) return 
+      const canvas = canvasRef.current
+      const context: CanvasRenderingContext2D | null  = canvas.getContext('2d')
+      if (!context) return;
+
+  },[])
 
   return (
     <>
-      <h1>Hello World</h1>
+      <canvas></canvas>
     </>
   )
 }
